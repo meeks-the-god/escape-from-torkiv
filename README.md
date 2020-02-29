b59c0c22590c0612137d9fca98d0e531b0bd94d8
Mekam Ledford <meledfor@northwestschool.org>
2020-02-29 12:36:56 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Escape from Torkiv
+
+A text horror game where you wake up in an unknown house.
+
+## Controls
+
+Keyboard
---
+++ src/main.py
@@ -0,0 +1,9 @@
+"""Entry file to the program escape from torkiv. See readme for details!"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
